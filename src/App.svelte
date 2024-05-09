4864549ec8a732dd4d1a4291d42d745c51e36cc3
Jared Whalen <jared.m.whalen@gmail.com>
2024-05-09 10:50:47 -0400

    update github pages

--- src/App.svelte
+++ src/App.svelte
@@ -14,14 +14,17 @@
 <!-- Outer div must have class 'chart-container' don't change -->
 <div class="chart-container">
   <h1 class="headline">
-    Mapping sewage sludge generation and land application at wastewater
-    treatment plants in 2023
+    Most sewage sludge from wastewater treatment plants is applied to land
   </h1>
   <p class="dek">
-    The EPA's ECHO database tracks the generation and land application of
-    biosolids at wastewater treatment plants. Often referred to as sewage
-    sludge, biosolids are spread on agricultural fields as fertilizer and poses
-    environmental risks, including PFAS contamination.
+    More than half of all sewage sludge generated at wastewater treatment
+    plants, sometimes called biosolids, is spread on land as fertilizer. Most of
+    the rest is buried in landfills or burned in incinerators. Sludge can also
+    pose environmental risks, including PFAS contamination. The U.S.
+    Environmental Protection Agency collects annual reports from roughly 2,500
+    larger facilities that generate sewage sludge. For more information on the
+    facilities and states included in the EPA's ECHO database, please visit the
+    <a target="_blank" href="https://www.epa.gov/biosolids/basic-information-about-biosolids#basics">'s website</a>.
   </p>
   <p class="sr-only">[altText]</p>
 
